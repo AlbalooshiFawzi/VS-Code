3f1ec25bb8eba9861f2942d5530267501e2dd964
AlbalooshiFawzi <falblooshi@uob.edu.bh>
2023-09-27 22:12:30 +0300

    added features

--- QuickStart.java
+++ QuickStart.java
@@ -1,6 +1,8 @@
 class QuickStart{
     public static void main (String[] args){
         System.out.println("Programming with Git");
-        System.out.println("First Steps ..");
+        System.out.println("First Steps .. modified");
+        System.out.println("added a new line");
+        System.out.println("another line of code");
     }
 } 
--- newJavaProg.java
+++ newJavaProg.java
@@ -1,6 +1,7 @@
-class QuickStart{
+class newJavaProg{
     public static void main (String[] args){
         System.out.println("Programming VS Code");
-        System.out.println("First Steps ..");
+        System.out.println("First Steps to program ..");
+        System.out.println("one more print message");
     }
 } 
