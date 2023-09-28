eb614df4711a821d8ce2d8f1459b090b63c60df8
AlbalooshiFawzi <falblooshi@uob.edu.bh>
2023-09-28 10:40:36 +0300

    new programm created

--- newJavaProg.java
+++ newJavaProg.java
@@ -1,3 +1,4 @@
+
 class newJavaProg{
     public static void main (String[] args){
         System.out.println("Programming VS Code");
---
+++ testsCheck.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+public class testsCheck {
+    @Test
+    public void test(){
+        assertEquals(2,1+1);
+    }
+}
