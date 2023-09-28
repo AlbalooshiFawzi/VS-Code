242e7c4e04b195fca6f1d4679e76426e299e2460
AlbalooshiFawzi <falblooshi@uob.edu.bh>
2023-09-28 18:28:44 +0300

    test programs developed

---
+++ CalculatorApp.java
@@ -0,0 +1,8 @@
+class CalculatorApp {
+     public static void main (String[] args){
+        CalculatorTest CT = new CalculatorTest();
+        CT.setUp();
+        CT.testAdd();
+        CT.testSubtract();
+    }
+}
--- newJavaProg.java
+++ newJavaProg.java
@@ -1,5 +1,6 @@
 
 class newJavaProg{
+
     public static void main (String[] args){
         System.out.println("Programming VS Code");
         System.out.println("First Steps to program ..");
